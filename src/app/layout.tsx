f691e3e384c881699c3fe34a69ecc4b4c6c1527f
DomCamillo2 <dominik@soballa.com>
2026-04-08 00:20:32 +0200

    feat(analytics): add global Google tag gtag.js snippet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,17 @@ export default function RootLayout({
   return (
     <html lang="de" suppressHydrationWarning className="h-full antialiased">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-04B6C4Y3NT"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-04B6C4Y3NT');
+            `,
+          }}
+        />
         <ThemeInitScript />
       </head>
       <body className="min-h-full flex flex-col bg-background text-foreground">
